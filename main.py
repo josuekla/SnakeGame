a36324d422b9647e89aa52b2569919272be0ad40
Josue_Klaysler <josueklayslerjk@gmail.com>
2024-12-22 15:24:55 -0300

    Set up initial game window

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import pygame
+import sys
 
 pygame.init()
-screen = pygame.display.set_mode((500, 600))
+screen = pygame.display.set_mode((400, 500))
+clock = pygame.time.Clock()
 
 while True:
-    #draw all elements from game
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+            sys.exit()
+    pygame.display.update()
+    clock.tick(60)
