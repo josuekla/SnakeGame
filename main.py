dece12db3e9acec55d91585f565f8f857f76d04d
Josue_Klaysler <josueklayslerjk@gmail.com>
2024-12-29 11:28:44 -0300

    Enhance snake graphics by adding tail rendering and adjusting body segments; update

--- main.py
+++ main.py
@@ -29,15 +29,25 @@ class Snake():
     
     def draw_snake(self):
         self.update_head_graphics()
+        self.update_tail_graphics()
 
         for index, block in enumerate(self.body):
+            print(index, block)
             x_pos = int(block.x * cells_size)
             y_pos = int(block.y * cells_size)
             block_rect = pygame.Rect(x_pos, y_pos, cells_size, cells_size)
             if index == 0:
                 screen.blit(self.head, block_rect)
+            elif index == len(self.body) - 1:
+                screen.blit(self.tail, block_rect)
             else:
-                pygame.draw.rect(screen, (100, 200, 10), block_rect)
+                previous_block = self.body[index + 1] - block
+                next_block = self.body[index - 1] - block
+                if previous_block.x == next_block.x:
+                    screen.blit(self.body_vertical, block_rect)
+                elif previous_block.y == next_block.y:
+                    screen.blit(self.body_horizontal, block_rect)
+            
         
     def update_head_graphics(self):
         head_relation = self.body[1] - self.body[0]
@@ -45,7 +55,13 @@ class Snake():
         elif head_relation == Vector2(-1, 0): self.head = self.head_right
         elif head_relation == Vector2(0, 1): self.head = self.head_up
         elif head_relation == Vector2(0, -1): self.head = self.head_down
-        
+    
+    def update_tail_graphics(self):
+        tail_relation = self.body[-2] - self.body[-1]
+        if tail_relation == Vector2(1, 0): self.tail = self.tail_left
+        elif tail_relation == Vector2(-1, 0): self.tail = self.tail_right
+        elif tail_relation == Vector2(0, 1): self.tail = self.tail_up
+        elif tail_relation == Vector2(0, -1): self.tail = self.tail_down
 
     def move_snake(self):
         if self.add_block == True:
@@ -108,8 +124,8 @@ class Main():
         
 
 pygame.init()
-cells_size = 35
-cells_number = 15
+cells_size = 40
+cells_number = 16   
 screen = pygame.display.set_mode((cells_size * cells_number, cells_size * cells_number))
 clock = pygame.time.Clock()
 apple = pygame.image.load('img/apple.png').convert_alpha()
@@ -119,7 +135,7 @@ main_game = Main()
 
 
 SCREEN_UPDATE = pygame.USEREVENT
-pygame.time.set_timer(SCREEN_UPDATE, 100)
+pygame.time.set_timer(SCREEN_UPDATE, 50)
 
 while True:
     
