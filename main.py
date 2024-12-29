0c55b3e3f9f45a961a03b3629c4c43fc556b150e
Josue_Klaysler <josueklayslerjk@gmail.com>
2024-12-29 11:40:55 -0300

    Add corner body segment graphics for the snake

--- main.py
+++ main.py
@@ -47,7 +47,15 @@ class Snake():
                     screen.blit(self.body_vertical, block_rect)
                 elif previous_block.y == next_block.y:
                     screen.blit(self.body_horizontal, block_rect)
-            
+                else:
+                    if previous_block.x == -1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == -1:
+                        screen.blit(self.body_tl, block_rect)
+                    elif previous_block.x == -1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == -1:
+                        screen.blit(self.body_bl, block_rect)
+                    elif previous_block.x == 1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == 1:
+                        screen.blit(self.body_tr, block_rect)
+                    elif previous_block.x == 1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == 1:
+                        screen.blit(self.body_br, block_rect)
         
     def update_head_graphics(self):
         head_relation = self.body[1] - self.body[0]
